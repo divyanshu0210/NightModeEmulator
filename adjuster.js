43ca91c76ce414acbf5c2690b495d5aca5e70e7d
DIVYANSHU <divyanshuagarwal2102002@gmail.com>
2021-06-09 13:43:05 +0530

    Txt appear on dark

--- adjuster.js
+++ adjuster.js
@@ -18,26 +18,53 @@ var output = document.getElementById("demo");
 
 var r = document.querySelector(':root');
    var p=document.getElementById("DarknessViewer");  
-
+var  font = document.getElementById("container_to_show");  
  /* var color=p.style.color;   */
-
+var limit = 80;
+var checkValue =0 ;
 slider.oninput = function(){
 	output.innerHTML = this.value;
 	  r.style.setProperty('--rgba',this.value/100);
 
-		 if (this.value >51){
+		 if (this.value >51 && this.value< limit ){
 	
+			checkValue=  0   ;
+			
 	  /*    showText(p);     */
 	p.style.color="white";
 	//  p.style.zIndex ="3";
 	
-	}    
+	font.style.zIndex ="-5";
 	
-	
+		}    
+		
+		else if (this.value == limit)
+			{
+			if (this.value > checkValue){
+			window.alert("  On increasing Darkness Background Visbility will Decrease .Only Text will be Visible !!");
+			}
+			}
+
+	else if(this.value> limit){
+		
+		 checkValue = this.value- (1/2);
+		
+		font.style.zIndex ="3";
+		/*  font.style.setProperty('--container_rgba'  , 1)  ;   */
+		
+		p.style.color="white";
+		
+		}
+		
 	else{
+		
+		checkValue =0;
 	
 		p.style.color= "black";
 	/*	p.style.zIndex="-5";   */
+	  
+	font.style.zIndex ="-5";
+	
 	}   
 	
 	
